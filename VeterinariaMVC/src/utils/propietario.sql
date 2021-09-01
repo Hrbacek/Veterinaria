8949004e08262293d93127f37f0a9572d2497348
Hrbacek <57201382+Hrbacek@users.noreply.github.com>
2021-09-01 08:40:48 -0500

    Add files via upload

---
+++ VeterinariaMVC/src/utils/propietario.sql
@@ -0,0 +1,11 @@
+CREATE TABLE propietario  (
+propUsuario   	  varchar(30)  PRIMARY KEY ,
+propApellido      varchar(30)  NOT NULL ,
+propNombre        varchar(30) NOT NULL ,
+propTelefono      char(25)    NOT NULL
+);
+INSERT INTO propietario(propUsuario,propApellido,propNombre,propTelefono) VALUES("vincent","Vanghogh","Vicente","3221234564");
+INSERT INTO propietario(propUsuario,propApellido,propNombre,propTelefono) VALUES("dantonito","Antony","Diego","3133151232");
+INSERT INTO propietario(propUsuario,propApellido,propNombre,propTelefono) VALUES("haylee","Hayo","Lee","3192212121");
+INSERT INTO propietario(propUsuario,propApellido,propNombre,propTelefono) VALUES("cristop","Rojas","Christopher","2927272 ext 333");
+INSERT INTO propietario(propUsuario,propApellido,propNombre,propTelefono) VALUES("JReina","Reina","Johan","+549 4984445413");
